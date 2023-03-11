42cd84ebf58cedc61c66a4f1fccc00be8c379916
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2023-03-11 23:09:55 +0530

    env added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const cors=require('cors')
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const codeforces = require("./routes/codeforces_router.js");
 const leetcode = require("./routes/leetcode_router.js");
@@ -7,7 +10,7 @@ const gitrouter = require("./routes/github_router.js");
 const codechef = require("./routes/codechef_router.js");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 ///routers
 
 //middlewares
@@ -20,6 +23,8 @@ app.use("/api/v1/codechef", codechef);
 app.use("/api/v1/leetcode", leetcode);
 app.use("/api/v1/github", gitrouter);
 
-app.listen(8080, () => {
-  console.log("server started on 8080");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}`);
 });
